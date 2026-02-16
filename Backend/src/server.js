9ac4c592cbd207255e3614b4e237f866d6d11ff9
Nishath-Syed <nishathsyed95@gmail.com>
2026-02-16 17:13:01 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoutes);   // 👈 IMPORTANT
+app.use("/auth", authRoutes);   
 
 app.get("/", (req, res) => {
   res.send("Backend is running...");
