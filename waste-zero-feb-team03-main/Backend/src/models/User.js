09ebe3ac4e63152d0cff65e6e61b19efb60483ec
VaishnaviMahendran <vaishumahendran34@gmail.com>
2026-02-22 23:48:28 +0530

    Change password minlength requirement

--- waste-zero-feb-team03-main/Backend/src/models/User.js
+++ waste-zero-feb-team03-main/Backend/src/models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Password is required'],
-    minlength: 8,
+    minlength: 6,
     select: false
   },
   role: {
